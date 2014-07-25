5bccfa4ef7b5b4f8e4ad6f71360989c1bcea67c5
Phillip Shipley <phillip_shipley@sil.org>
2014-07-25 16:27:57 -0400

    updates to composer and configure recipes

--- silphp/recipes/composer.rb
+++ silphp/recipes/composer.rb
@@ -1,8 +1,10 @@
 # Update composer dependencies
 node['deploy'].each do |appname, deploy|
-    update_composer do
-        path "#{deploy['deploy_to']}/#{deploy['composer']['dir']}"
-        self_update deploy['composer']['self_update']
-        as_update deploy['composer']['as_update']
+    if ( deploy['composer'] && deploy['composer'].is_a?(::Hash) )
+        update_composer do
+            path "#{deploy['deploy_to']}/#{deploy['composer']['dir']}"
+            self_update deploy['composer']['self_update']
+            as_update deploy['composer']['as_update']
+        end
     end
 end
--- silphp/recipes/configure.rb
+++ silphp/recipes/configure.rb
@@ -1,29 +1,20 @@
 node['deploy'].each do |application, deploy|
-  
-  # write out local.php
-  template 'local.php' do
-    source 'array.php.erb'
-    path "#{deploy['deploy_to']}/#{deploy['config_path']}/local.php"
-    mode '0664'
-    owner deploy['user']
-    group deploy['group']
-    variables(
-      :data => Silphp::Helper.hash_to_array(deploy['local']),
-    )
-    #only_if defined? deploy['local']
-  end
 
-  # write out params.php
-  template 'params.php' do
-    source 'array.php.erb'
-    path "#{deploy['deploy_to']}/#{deploy['config_path']}/params.php"
-    mode '0664'
-    owner deploy['user']
-    group deploy['group']
-    variables(
-      :data => Silphp::Helper.hash_to_array(deploy['params']),
-    )
-    #only_if defined? deploy['params']
+  if (deploy['config_files'] && deploy['config_files'].is_a?(::Hash))
+    deploy['config_files'].each do |file, data|
+      template "config file #{data['filename']}" do
+        source 'array.php.erb'
+        path "#{deploy['deploy_to']}/#{deploy['config_path']}/#{data['filename']}"
+        mode '0664'
+        owner deploy['user']
+        group deploy['group']
+        variables(
+          :data => Silphp::Helper.hash_to_array(data['content']),
+        )
+      end
+
+    end
+    
   end
 
 end
