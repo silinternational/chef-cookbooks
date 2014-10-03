4869d071af45815572f2146ef5ac52da977ffa7a
Phillip Shipley <phillip_shipley@sil.org>
2014-10-03 16:55:55 -0400

    updated some recipes for describingsil

--- describingsil/recipes/default.rb
+++ describingsil/recipes/default.rb
@@ -1,7 +1,7 @@
 # Recipe installs and configures dependencies for Addressbook
 
 # Required packages
-packages = ["git", "httpd", "php", "php-mcrypt", "php-xml", "php-mbstring", "php-pdo"]
+packages = ["git", "httpd", "php", "php-mcrypt", "php-xml", "php-mbstring", "php-pdo", "php-mysql"]
 
 packages.each do |name|
     package name do
@@ -22,6 +22,13 @@ node['deploy'].each do |appname, deploy|
         owner "apache"
         group "apache"
         mode "0775"
+    end
+
+    # Make sure yiic script is executable
+    file "#{deploy['deploy_to']}#{deploy['aws_extra_path']}/#{deploy['yii_dir']}/yiic" do
+        owner "root"
+        group "root"
+        mode "0755"
     end
 
     # Create simplesaml symlink if needed
--- silapache2/metadata.rb
+++ silapache2/metadata.rb
@@ -1,7 +1,10 @@
 name "silapache2"
 description "Custom apache2 related recipes and templates"
 license "MIT"
-version "0.0.1"
+version "0.0.2"
 maintainer "SIL International, GTIS"
 
 recipe "silapache2::default", "Empty default recipe"
+recipe "silapache2::deploy_vhosts", "Create SSL cert files and vhost entries from entries in node['deploy']"
+
+depends "apache2"
